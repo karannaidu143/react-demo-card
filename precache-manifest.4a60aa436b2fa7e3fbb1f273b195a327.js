589f8c9886bd4196998ba2486864566356561fca
karannaidu143 <karan.cs18@bmsce.ac.in>
2020-08-18 09:12:39 +0530

    Updates

---
+++ precache-manifest.4a60aa436b2fa7e3fbb1f273b195a327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1b513f7a516ec443b72313a84b0e59",
+    "url": "/react-demo-card/index.html"
+  },
+  {
+    "revision": "2b9fbf9b3a9ed7ccf233",
+    "url": "/react-demo-card/static/css/main.6e06b2a1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-demo-card/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-card/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9fbf9b3a9ed7ccf233",
+    "url": "/react-demo-card/static/js/main.8ccd32c7.chunk.js"
+  },
+  {
+    "revision": "d9c4f0b6f99d7b45998a",
+    "url": "/react-demo-card/static/js/runtime-main.21935847.js"
+  }
+]);
